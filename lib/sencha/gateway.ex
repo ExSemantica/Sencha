06d9e17e71c3063cb0da3a385b5623c56538a724
Roland Metivier <metivier.roland@chlorophyt.us>
2025-02-12 20:23:14 -0600

    self should work here

--- lib/sencha/gateway.ex
+++ lib/sencha/gateway.ex
@@ -3,7 +3,7 @@ defmodule Sencha.Gateway do
   Bridges Sencha with ExSemantica's database
   """
   def fastest_node do
-    Node.list() |> Enum.map(&send({Exsemantica.Gateway, &1}, {:ping, Node.self()}))
+    Node.list() |> Enum.map(&send({Exsemantica.Gateway, &1}, {:ping, self()}))
 
     receive do
       {ExSemantica.Gateway, fastest, :pong} ->
