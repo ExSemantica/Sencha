3369f74ee9ec0a4c112d254a1acb46f63672853c
Roland Metivier <metivier.roland@chlorophyt.us>
2025-02-14 21:32:31 -0600

    rearrange delimiters for channel casts

--- lib/sencha/channel.ex
+++ lib/sencha/channel.ex
@@ -73,7 +73,7 @@ defmodule Sencha.Channel do
   @impl true
   def handle_cast(
         {:part,
-         {user_socket, user_state = %Sencha.Handler.UserState{user_process: user_process}, reason}},
+         {user_socket, user_state = %Sencha.Handler.UserState{user_process: user_process}}, reason},
         state = %{users: users, channel: channel}
       ) do
     if {user_socket, user_process} in users do
@@ -106,8 +106,8 @@ defmodule Sencha.Channel do
   @impl true
   def handle_cast(
         {:talk,
-         {user_socket, user_state = %Sencha.Handler.UserState{user_process: user_process},
-          message}},
+         {user_socket, user_state = %Sencha.Handler.UserState{user_process: user_process}},
+          message},
         state = %{users: users, channel: channel}
       ) do
     if {user_socket, user_process} in users do
