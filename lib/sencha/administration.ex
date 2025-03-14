4a19a540766dea7aaf062c1f8aa2ad087bbc6049
Roland Metivier <metivier.roland@chlorophyt.us>
2025-03-13 19:40:37 -0500

    document what I put in

--- lib/sencha/administration.ex
+++ lib/sencha/administration.ex
@@ -2,6 +2,10 @@ defmodule Sencha.Administration do
   @moduledoc """
   Utilities for administering IRC
   """
+
+  @doc """
+  Convenience for `Sencha.UserSupervisor.broadcast_wallops/1`.
+  """
   def broadcast_wallops(message) do
     Sencha.UserSupervisor.broadcast_wallops(message)
   end
