c6444b5fa63f94e47da7ccd5934d3676b0df48bf
Roland Metivier <metivier.roland@chlorophyt.us>
2025-03-16 16:29:54 -0500

    user process not unregistering

--- lib/sencha/handler.ex
+++ lib/sencha/handler.ex
@@ -560,8 +560,9 @@ defmodule Sencha.Handler do
           |> Sencha.Message.encode()
         )
       end
+
+      Sencha.UserSupervisor.terminate_child(user_process)
     end
-    Sencha.UserSupervisor.terminate_child(user_process)
 
     :ok
   end
