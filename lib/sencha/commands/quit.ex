18b0b7cfb05543526813a5fc6a676aa06189f21c
Roland Metivier <metivier.roland@chlorophyt.us>
2026-03-06 10:51:08 -0600

    attempt to actually handle QUIT message

---
+++ lib/sencha/commands/quit.ex
@@ -0,0 +1,40 @@
+defmodule Sencha.Commands.Quit do
+  @moduledoc """
+  Handle 'QUIT' IRCv3 commands
+  """
+
+  def handle_irc(
+        _pid,
+        _packet = %Sencha.Message{trailing: nil},
+        {_socket,
+         _state = %Sencha.Handler.UserState{
+           user_process: user,
+           authentication_state: :ok
+         }}
+      ) do
+    Sencha.User.disconnect(user, "Client Quit")
+
+    :ok
+  end
+  def handle_irc(
+        _pid,
+        _packet = %Sencha.Message{trailing: reason},
+        {_socket,
+         _state = %Sencha.Handler.UserState{
+           user_process: user,
+           authentication_state: :ok
+         }}
+      ) do
+    Sencha.User.disconnect(user, "Client Quit (#{reason})")
+
+    :ok
+  end
+
+  def handle_irc(
+        _pid,
+        _packet,
+        {_socket, _state}
+      ) do
+    :ok
+  end
+end
--- lib/sencha/handler.ex
+++ lib/sencha/handler.ex
@@ -417,6 +417,13 @@ defmodule Sencha.Handler do
   end
 
   @impl GenServer
+  def handle_info({:irc, packet = %Sencha.Message{command: "QUIT"}}, {socket, state}) do
+    Sencha.Commands.Quit.handle_irc(self(), packet, {socket, state})
+
+    {:noreply, {socket, state}}
+  end
+
+  @impl GenServer
   def handle_info(
         {:irc, %Sencha.Message{command: "ERROR", trailing: nil}},
         {socket, state}
