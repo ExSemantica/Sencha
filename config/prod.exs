da1e92e7700d0d779a3005fe62a6c70d4306cac7
Roland Metivier <metivier.roland@chlorophyt.us>
2026-04-02 20:20:18 -0500

    tighten ping timeout timing

--- config/prod.exs
+++ config/prod.exs
@@ -2,8 +2,8 @@ import Config
 
 # Timeouts are in milliseconds
 config :sencha, auth_timeout: 15_000
-config :sencha, ping_timeout: 120_000
-config :sencha, ping_timeout_hard: 5_000
+config :sencha, ping_timeout: 60_000
+config :sencha, ping_timeout_hard: 10_000
 
 # These are in seconds
 config :sencha, oper_duration: 30
